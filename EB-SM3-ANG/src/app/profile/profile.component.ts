4536d5e79dc8e171a76fd46596105d1db5a0f243
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2020-12-04 09:08:51 +0100

    User password change + item purchase

--- EB-SM3-ANG/src/app/profile/profile.component.ts
+++ EB-SM3-ANG/src/app/profile/profile.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Component, OnInit, TemplateRef} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
 import {UserService} from './shared/user.service';
 import {AuthenticationService} from '../shared/services/authentication.service';
 import {User} from './shared/user';
+import {Customer} from './shared/customer';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Location} from '@angular/common';
+import {BsModalRef, BsModalService} from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-profile',
@@ -21,24 +25,130 @@ export class ProfileComponent implements OnInit {
     cityName: new FormControl('', [Validators.required]),
   });
 
+  updateForm = new FormGroup({
+    oldPassword: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(8)])
+  }, {validators: [this.passwordConfirming]});
 
-  loading: boolean = false;
+  passwordConfirming(c: AbstractControl): { invalid: boolean } {
+    if (c.get('password').value !== c.get('passwordConfirm').value) {
+      return {invalid: true};
+    }}
+
+
+  loading: boolean = true;
+  formLoading: boolean = false;
   foundUserInfo: boolean = false;
   error: string = '';
 
-  user: User = null
+  user: User = null;
+  modalRef: BsModalRef;
+  passwordUpdateLoading: boolean = false;
+  passwordError: string = '';
 
-  constructor(private userService: UserService, private authService: AuthenticationService) { }
+
+  constructor(private userService: UserService, private authService: AuthenticationService,
+              private router: Router, private route: ActivatedRoute,
+              private location: Location, private modalService: BsModalService) { }
 
   ngOnInit(): void {
+    this.loadUser();
   }
 
   loadUser(): void{
     const userID = this.authService.getID();
-    this.userService.getUserById(userID).subscribe((user) => {this.user = user}, (error) => {this.error = error.error});
+    this.userService.getUserById(userID).subscribe((user) => {this.user = user}, (error) => {this.error = error.error; this.loading = false;},
+      () => {
+        if (this.user.customer === null){this.foundUserInfo = false; this.loading = false; }
+        else{
+          this.initialText();
+        }});
   }
 
+  initialText(): void{
+
+      this.userForm.patchValue({
+        fname: this.user.customer.firstName,
+        lname: this.user.customer.lastName,
+        email: this.user.customer.email,
+        pNumber: this.user.customer.phoneNumber,
+        streetName: this.user.customer.streetName,
+        postalCode: this.user.customer.postalCode,
+        cityName: this.user.customer.cityName
+      });
+      this.foundUserInfo = true;
+      this.loading = false;
+    }
+
   createCustomerInfo(): void
-  {}
+  {
+    this.formLoading = true;
+
+    const userData = this.userForm.value;
+    const costumer: Customer = {
+      id: 0,
+      firstName: userData.fname,
+      lastName: userData.lname,
+      email: userData.email,
+      phoneNumber: userData.pNumber,
+      streetName: userData.streetName,
+      postalCode: userData.postalCode,
+      cityName: userData.cityName,
+      user: this.user
+    }
+
+    this.userService.createUserCustomer(costumer).subscribe(() => {},
+      (error) => {this.error = error.error; this.formLoading = false;
+        if(error.status === 401){this.router.navigate(['/login']);}},
+      () => {this.router.navigate(['/home']);});
+  }
+
+  updateCustomerInfo(): void
+  {
+    this.formLoading = true;
+    const userData = this.userForm.value;
+
+    this.user.customer.firstName = userData.fname;
+    this.user.customer.lastName = userData.lname;
+    this.user.customer.email = userData.email;
+    this.user.customer.phoneNumber = userData.pNumber;
+    this.user.customer.streetName = userData.streetName;
+    this.user.customer.postalCode = userData.postalCode;
+    this.user.customer.cityName = userData.cityName;
+
+    const userID = this.authService.getID();
+
+    this.userService.updateUserCustomer(this.user.customer, userID).subscribe(() => {},
+      (error) => {this.error = error.error; this.formLoading = false;
+        if(error.status === 401){this.router.navigate(['/login']);}},
+      () => {this.router.navigate(['/home']);});
+  }
+
+  updatePassword(): void{
+
+    this.passwordUpdateLoading = true;
+    const passwordData = this.updateForm.value;
+
+    const passwordModel:any = {
+      oldPassword: passwordData.oldPassword,
+      newPassword: passwordData.password
+    }
+
+    const userID = this.authService.getID();
+
+    this.userService.updateUserPassword(userID, passwordModel).subscribe(() => {},
+      (error) => {this.passwordError = error.error; console.log(error);this.passwordUpdateLoading = false;
+        if(error.status === 401){this.router.navigate(['/login']);}},
+      () => {this.modalRef.hide(); this.passwordUpdateLoading = false;});
+  }
+
+  openUpdateModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  goBack(): void{
+    this.location.back();
+  }
 
 }
--- EB-SM3-ANG/src/app/side-bar/side-bar.component.ts
+++ EB-SM3-ANG/src/app/side-bar/side-bar.component.ts
@@ -1,6 +1,11 @@
 import {Component, HostListener, OnInit} from '@angular/core';
 import {ShoppingCartService} from '../shared/services/shopping-cart.service';
 import {OrderItem} from '../shared/services/orderItem';
+import {AuthenticationService} from '../shared/services/authentication.service';
+import {Router} from '@angular/router';
+import {UserService} from '../profile/shared/user.service';
+import {Customer} from '../profile/shared/customer';
+import {Order} from '../shared/services/order';
 
 @Component({
   selector: 'app-side-bar',
@@ -13,7 +18,11 @@ export class SideBarComponent implements OnInit {
     this.ngOnDestroy();
   }
 
-  constructor(public shoppingService: ShoppingCartService) { }
+  orderCreateLoad: boolean = false;
+  error: string = '';
+
+  constructor(public shoppingService: ShoppingCartService, private authService: AuthenticationService,
+              private router: Router, private userService: UserService) { }
 
   ngOnInit(): void {
   }
@@ -31,6 +40,53 @@ export class SideBarComponent implements OnInit {
       this.shoppingService.removeItem(orderItem.item);
     }
   }
+
+  orderProducts(): void{
+
+    this.orderCreateLoad = true;
+
+    let date = new Date();
+    date.setTime(date.getTime() + 2*60*60*1000);
+
+    let listItems: any[] = [];
+    this.shoppingService.getItems().forEach((orderItem) => {
+      listItems.push({beerID: orderItem.item.id, amount: orderItem.quantity})
+    })
+
+
+
+    const userID = this.authService.getID();
+    if(userID === null){this.router.navigate(['/login']); this.orderCreateLoad = false;}
+
+    this.userService.getCustomerById(userID).subscribe((customer) => {
+      if(customer === null){this.router.navigate(['/profile']);}
+
+      const order: Order = {
+        id: 0,
+        accumulatedPrice: this.shoppingService.calculateTotalPrice(),
+        customer: customer,
+        orderBeers: listItems,
+        orderCreated: new Date(date)
+      }
+
+      this.userService.purchaseCart(order).subscribe((order) => {
+        this.orderCreateLoad = false;
+        this.shoppingService.clearCart();
+        this.closeShoppingCart();
+        //this.router.navigate(['/order/' + order.id]);
+        },
+        (error) => {
+        if (error.status === 400){
+          //recalculate items in case of stock change
+          this.shoppingService.loadCart();
+        }
+        if (error.status === 401){this.router.navigate(['/login']);}
+          this.orderCreateLoad = false;
+          this.error = error.error;
+      })
+    }, (error) => {this.orderCreateLoad = false; this.router.navigate(['/profile']);})
+  }
+
 
   getCartStatus(): boolean{
     return this.shoppingService.isClosed;
